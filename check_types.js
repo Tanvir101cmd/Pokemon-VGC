d6676b3a638768c78d98f0368e567df5e88233a2
ZARRAFUZZAMAN CHOUDHURY <156108533+ZARGIACK@users.noreply.github.com>
2025-12-31 20:51:47 +0600

    Add files via upload

---
+++ check_types.js
@@ -0,0 +1,14 @@
+const db = require('./db');
+
+const sid = process.argv[2] || 6;
+const sql = 'SELECT * FROM pokemon_types WHERE pokemon_sid = ?';
+
+db.query(sql, [sid], (err, rows) => {
+  if (err) {
+    console.error('Query error:', err);
+    process.exit(1);
+  }
+  console.log('Rows for pokemon_sid=' + sid + ':');
+  console.log(JSON.stringify(rows, null, 2));
+  process.exit(0);
+});
